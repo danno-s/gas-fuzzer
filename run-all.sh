eef2c13785a8319aafdd5de685a4149a1a5bc824
Daniel Soto <danielsoto.3004@gmail.com>
2019-07-08 17:25:51 -0400

    envía output de script a /dev/null

--- run-all.sh
+++ run-all.sh
@@ -4,7 +4,7 @@ echo "starting..."
 
 for file in etherscan/*
 do
-    python3 gas_fuzz/gas_fuzz.py -tx 15 -s 10 $file
+    python3 gas_fuzz/gas_fuzz.py -tx 15 -s 10 $file >> /dev/null
     if [ $? -ne 0 ]
     then
         echo "failure on $file" >> exec.log
